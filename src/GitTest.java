e12cfa10fe854964dbd32e869402056a5a31b348
OlhaBahno <olya.bagno@gmail.com>
2018-10-16 17:12:39 +0300

    two functions: multiply and all odds

--- src/GitTest.java
+++ src/GitTest.java
@@ -23,6 +23,32 @@ public class GitTest {
         this.size = size;
     }
 
+
+
+    //Multiply
+    public int mulp(){
+
+        int result = 1;
+        for (int i=0; i<size; i++){
+            result*=arr[i];
+        }
+        return result;
+    }
+
+    //amount of odds
+    public int odds(){
+        int result = 0;
+
+        for(int i=0;i<size;i++){
+            if(arr[i]%2 != 0){
+                result++;
+            }
+        }
+        return result;
+    }
+
+
+
     @Override
     public String toString() {
         return "GitTest{" +
